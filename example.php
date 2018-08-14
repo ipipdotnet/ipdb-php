878b66a0d41a7493fbda0a6668919c6bdda0ef59
小宝 <ilsanboo@gmail.com>
2018-08-14 18:04:46 +0800

    update

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . '/src/ipip/Info.php';
-require_once __DIR__ . '/src/ipip/Database.php';
+require_once __DIR__ . '/src/ipip/db/Info.php';
+require_once __DIR__ . '/src/ipip/db/Reader.php';
 
-$db = new ipip\Database('c:/work/tiantexin/framework/library/ip/mydata6.ipdb');
+$db = new ipip\db\Reader('c:\work\tiantexin\bb\v6\mydatavipday2.ipdb');
 $loc = $db->find("2001:250:200::");
 $map = $db->findMap("2001:250:200::");
 $obj = $db->findInfo("2001:250:200::");
