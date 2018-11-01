935e99f62d753d5856627965d0735c0c1afc69f9
小宝 <ilsanboo@gmail.com>
2018-11-01 17:46:13 +0800

    update

--- src/ipip/db/BaseStation.php
+++ src/ipip/db/BaseStation.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @site https://www.ipip.net
+ * @desc Parse IP library in ipdb format
+ * @copyright IPIP.net
+ */
 namespace ipip\db;
 
 class BaseStation
--- src/ipip/db/CityInfo.php
+++ src/ipip/db/CityInfo.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @site https://www.ipip.net
+ * @desc Parse IP library in ipdb format
+ * @copyright IPIP.net
+ */
+
 namespace ipip\db;
 
 class CityInfo
